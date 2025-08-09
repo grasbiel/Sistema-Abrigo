b474a13b564c38eeb1d58eed15513f41bd9e82f2
grasbiel <bzsilva1998@gmail.com>
2025-08-09 10:58:30 -0300

    Urls config: Orgsanização do arquivo

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,18 @@
-# config/urls.py
+# backend/config/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+from core.views import MyTokenObtainPairView
 
 urlpatterns = [
+    # 1. Rota para a interface de administração do Django
     path('admin/', admin.site.urls),
-    # Rotas da API
+    
+    # 2. Rota que delega tudo que começa com 'api/' para o arquivo urls.py da app 'core'
     path('api/', include('core.urls')),
-    # Rotas de Autenticação
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # 3. Rotas para autenticação com Token JWT
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
